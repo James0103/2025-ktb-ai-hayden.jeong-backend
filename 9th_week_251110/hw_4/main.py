f0b877a54a3527dfcb192f6525b9221ef8f6f09d
James0103 <jameshhjung@gmail.com>
2025-12-02 17:40:58 +0900

    Update gitignore and project structure

--- 9th_week_251110/hw_4/main.py
+++ 9th_week_251110/hw_4/main.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI
-import uvicorn
-from router.user_router import user_router
-from router.post_router import post_router
-from router.reply_router import reply_router
-from router.misc_router import misc_router
+# from fastapi import FastAPI
+# import uvicorn
+# from router.user_router import user_router
+# from router.post_router import post_router
+# from router.reply_router import reply_router
+# from router.misc_router import misc_router
 
-app = FastAPI()
+# app = FastAPI()
 
-app.include_router(user_router)
-app.include_router(post_router)
-app.include_router(reply_router)
-app.include_router(misc_router)
+# app.include_router(user_router)
+# app.include_router(post_router)
+# app.include_router(reply_router)
+# app.include_router(misc_router)
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", reload=True)
+
+import torch
+
+print(torch.softmax())
